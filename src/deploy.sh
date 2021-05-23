b9d4a120c0743367dccd3b2b56e77ecf15bff7c6
nearlySplat <splatterxl@outlook.ie>
2021-05-23 20:31:16 +0100

    root

---
+++ src/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eo pipefail
+
+cd $GITHUB_WORKSPACE
+
+echo "::[notice] # Run the build"
+npm run docs:build
+
+# Initialise some useful variables
+REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+BRANCH_OR_TAG=`awk -F/ '{print $2}' <<< $GITHUB_REF`
+CURRENT_BRANCH=`awk -F/ '{print $NF}' <<< $GITHUB_REF`
+
+if [ "$BRANCH_OR_TAG" == "heads" ]; then
+  SOURCE_TYPE="branch"
+else
+  SOURCE_TYPE="tag"
+fi
+
+echo "::[notice] # Checkout the repo so we can build docs and push to it"
+TARGET_BRANCH="docs"
+git clone $REPO out
+cd out
+
+# Check if the target branch exists
+if [ ! "$(git ls-remote origin $TARGET_BRANCH)" ]; then
+	echo "::[notice] # Branch $TARGET_BRANCH not found, exiting action"
+	exit
+fi
+
+git checkout $TARGET_BRANCH
+echo "::[notice] # Move the generated JSON file to the newly-checked-out repo, to be committed and pushed"
+mv ../docs $CURRENT_BRANCH
+echo "::[notice] # Commit and push"
+git add .
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git commit -m "Docs build for ${SOURCE_TYPE} ${CURRENT_BRANCH}: ${GITHUB_SHA}" || true
+git push origin $TARGET_BRANCH
