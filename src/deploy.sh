cad056ecea4da1b413a4e85d309e48556e37ea82
nearlySplat <splatterxl@outlook.ie>
2021-05-23 21:22:18 +0100

    Update deploy.sh

--- src/deploy.sh
+++ src/deploy.sh
@@ -31,7 +31,7 @@ fi
 
 git checkout $TARGET_BRANCH
 echo "::[notice] # Move the generated JSON file to the newly-checked-out repo, to be committed and pushed"
-mv ../docs $CURRENT_BRANCH
+mv ../docs .
 echo "::[notice] # Commit and push"
 git add .
 git config user.name "${GITHUB_ACTOR}"
